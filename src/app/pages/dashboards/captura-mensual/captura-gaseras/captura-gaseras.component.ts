ac3d6ea0ba6cc26594ff47aeb3ccb27f2cbeb88d
desarrollo01Cob <desarrollo_01@cobama.com.mx>
2025-03-07 18:16:11 -0600

    Feat: Captura energeticos from excel

--- src/app/pages/dashboards/captura-mensual/captura-gaseras/captura-gaseras.component.ts
+++ src/app/pages/dashboards/captura-mensual/captura-gaseras/captura-gaseras.component.ts
@@ -20,44 +20,37 @@ import { EnergeticosGaseras } from "src/app/core/models/dashboard/energeticos-ga
 })
 export class CapturaGaserasComponent implements OnInit {
   public dataEmpresas: EnergeticosGaseras[]; //alacena los datos recuperados del mes
-
   public catEmpresas: CatEmpresas[]; //Empresas (Gaseras)
 
   public mostrar: boolean = false;
-
   public formDatosEnergeticos: FormGroup; //Formulario dinámico
-
   public meses = dataMeses; //meses en el select
-
+  public isLoad = false; //bandera para mostrar bandera o spiner
   private fecha = new Date();
-
   public mes = this.fecha.getMonth();
-
   public anio = this.fecha.getFullYear();
-
   public isDisabled: boolean = false;
-
-  public existInfo: boolean = true; //Define si existe información capturada en el periodo
- 
-  //Modelo para generar los inputs
-  private modelInputs = {
-    
-    venta_litros: "",
-    ventas: "",
-    eficiencia: "",
-    gasto: "",
-    utilidad_bruta: "",
-    ubo: "",
-    personal: "",
-    uno: "",
-  };
+  public existInfo: boolean = false; //Define si existe información capturada en el periodo
+  public accion: any; // Texto del botón guardar/ actualizar
+  public iconoAccion: any; // Icono del botón guardar/ actualizar
+  public selecMes: boolean = false; // bandera para saber si el Select del mes tiene un valor valido
+  public selecAnio: boolean = false; // bandera para saber si el Select del año tiene un valor valido
+  public hasDatos: boolean = false; //Indica si se ha pagado algo desde el portapapeles
+  public headers: string[] = []; // headers obtenidos desde el contenido copiado
+  public showInstructions: boolean = true; //Bandera para mostrar u ocultar instrucciones
+  public dataMesAgencias: any;
+  public showBtnAccion: boolean = false;
+  public verBtnConsulta: boolean = true;
+  public showTable: boolean = false; //Bandera para mostrar u ocultar tabla
+  public dataToSend: any;
+  public lastIndex: any;
 
   constructor(
     private catEmpresasService: CatEmpresasService,
     public alertService: AlertErrorService,
     public formBuilder: FormBuilder,
     private energerticosGaseras: EnergeticosGaserasService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.getAllEmpresas();
@@ -72,7 +65,6 @@ export class CapturaGaserasComponent implements OnInit {
       (data: ResponseCatEmpresas) => {
         if (data.success) {
           this.catEmpresas = data.data;
-          // this.buildFormDatosEnergeticos();
         } else {
           this.alertService.alertError(data.message, data.success);
         }
@@ -83,121 +75,54 @@ export class CapturaGaserasComponent implements OnInit {
     );
   }
 
-  indexTotals: any;
-
-  /**
-   * Genera el formulario
-   * Flujo 1: En base a datos recuperados del servidor (Si hay datos de captura)
-   * Flujo 2: En base a un modelo y a las estaciones (Si no existen datos de captura)
-   */
-  private buildFormDatosEnergeticos() {
-    const fields = {};
-
-    this.formDatosEnergeticos = this.formBuilder.group({});
-
-    if (this.existInfo) {
-      Object.entries(this.dataEmpresas).forEach((data) => {
-        for (const field of Object.keys(this.modelInputs)) {
-          this.formDatosEnergeticos.addControl(
-            field + "_" + data[1].id,
-            this.formBuilder.control(data[1][field], Validators.required)
-          );
-          this.indexTotals = this.dataEmpresas.length - 1;
-        }
-      });
-    } else {
-      Object.entries(this.catEmpresas).forEach((data) => {
-        for (const field of Object.keys(this.modelInputs)) {
-          this.formDatosEnergeticos.addControl(
-            field + "_" + data[1].id,
-            this.formBuilder.control(0, Validators.required)
-          );
-        }
-      });
-    }
-
-    this.mostrar = true;
-    this.isDisabled = false;
-  }
   /**
    * Guarda y actualiza los datos capturados en
    * el formulario
    */
   public saveInfo() {
-    // console.warn(this.formDatosEnergeticos.value);
-    if (this.formDatosEnergeticos.valid) {
-      const formData = this.formDatosEnergeticos.value;
-      const datos = [];
-
-      const fecha = `${this.anio}-${this.mes.toString().padStart(2, "0")}-01`;
-
-      Object.entries(formData).forEach(([key, value]) => {
-        //  Separa la columna del id
-        const lastIndex = key.length;
-        const lastGuion = key.lastIndexOf("_");
-        const sucursales_id = key.substring(lastGuion + 1, lastIndex);
-        const field = key.substring(0, lastGuion);
-
-        let datosEmpresa = datos.find(
-          (item) => item.sucursales_id === Number(sucursales_id)
-        );
-        if (
-          !datosEmpresa
-          // && !isNaN(Number(sucursales_id))
-        ) {
-          datosEmpresa = {
-            sucursales_id: Number(sucursales_id),
-            fecha: fecha,
-            isNew: !this.existInfo, //NOTA: Define la manera en la que se guardara o actualizara los datos
-          };
-          datos.push(datosEmpresa);
-        }
-        datosEmpresa[field] = value;
-      });
-      Swal.fire({
-        title: "¿Estas seguro?",
-        text: "Deseas capturar estos datos",
-        icon: "warning",
-        confirmButtonText: "SI",
-        showCancelButton: true,
-        cancelButtonText: "NO",
-        customClass: {
-          confirmButton: "btn btn-success px-4",
-          cancelButton: "btn btn-danger ms-2 px-4",
-        },
-        buttonsStyling: false,
-      }).then((result) => {
-        if (result.value) {
-          this.energerticosGaseras.save(datos).subscribe((response) => {
-            if (response.status === "success") {
-              Swal.fire({
-                title: "Listo",
-                text: "Datos Guardados correctamente",
-                buttonsStyling: false,
-                icon: "success",
-                customClass: {
-                  confirmButton: "btn btn-success px-4",
-                  cancelButton: "btn btn- ms-2 px-4",
-                },
-              });
-              this.getInfoMes();
-            } else {
-              Swal.fire({
-                title: "Error",
-                text: response.message,
-                buttonsStyling: false,
-                icon: "error",
-                customClass: {
-                  confirmButton: "btn btn-success px-4",
-                  cancelButton: "btn btn- ms-2 px-4",
-                },
-              });
-              this.getInfoMes();
-            }
-          });
-        }
-      });
-    }
+    Swal.fire({
+      title: "¿Estas seguro?",
+      text: "Deseas capturar estos datos",
+      icon: "warning",
+      confirmButtonText: "SI",
+      showCancelButton: true,
+      cancelButtonText: "NO",
+      customClass: {
+        confirmButton: "btn btn-success px-4",
+        cancelButton: "btn btn-danger ms-2 px-4",
+      },
+      buttonsStyling: false,
+    }).then((result) => {
+      if (result.value) {
+        this.energerticosGaseras.save(this.dataToSend).subscribe((response) => {
+          if (response.status === "success") {
+            Swal.fire({
+              title: "Listo",
+              text: "Datos Guardados correctamente",
+              buttonsStyling: false,
+              icon: "success",
+              customClass: {
+                confirmButton: "btn btn-success px-4",
+                cancelButton: "btn btn- ms-2 px-4",
+              },
+            });
+            this.getInfoMes();
+          } else {
+            Swal.fire({
+              title: "Error",
+              text: response.message,
+              buttonsStyling: false,
+              icon: "error",
+              customClass: {
+                confirmButton: "btn btn-success px-4",
+                cancelButton: "btn btn- ms-2 px-4",
+              },
+            });
+            this.getInfoMes();
+          }
+        });
+      }
+    });
   }
 
   /**
@@ -208,137 +133,142 @@ export class CapturaGaserasComponent implements OnInit {
   public getInfoMes() {
     this.isDisabled = true;
     this.dataEmpresas = [];
+    this.existInfo = false;
+    this.isLoad = false;
+    this.showTable =true;
 
     this.energerticosGaseras.get(this.mes, this.anio).subscribe((data) => {
       if (data.success && data.data.length > 1) {
         this.dataEmpresas = data.data;
+        this.isLoad = true;
         this.existInfo = true;
+        this.hasDatos = false;
+        this.accion = "Actualizar";
+        this.iconoAccion = "bx bx-pencil";
+        this.verBtnConsulta = false;
+        this.showTable = true;
+        this.isDisabled = false;
+        this.lastIndex = (this.dataEmpresas.length-1)
       } else {
         this.alertService.alertError(
           "No hay información captura en el periodo seleccionado",
           false
         );
+        this.verBtnConsulta = false;
+        this.showTable = true;
+        this.hasDatos = false;
+        this.dataMesAgencias = [];
+        this.isLoad = true;
+        this.iconoAccion = "bx bxs-save";
+        this.accion = "Guardar";
         this.dataEmpresas = [];
         this.getAllEmpresas();
         this.existInfo = false;
+        this.isDisabled = false;
       }
-
-      this.buildFormDatosEnergeticos();
-
-      // if (data.success) {
-      //   this.dataEmpresas = data.data;
-
-      //   if (Object.entries(this.dataEmpresas).length > 1) {
-      //     this.buildFormDatosEnergeticos();
-      //     this.existInfo = true;
-      //   } else {
-      //     this.alertService.alertError('No hay información captura en el periodo seleccionado', false);
-      //     this.dataEmpresas = [];
-      //     this.existInfo = false;
-      //     this.getAllEmpresas();
-      //   }
-      // } else {
-      //   this.alertService.alertError(data.message, false);
-      // }
-    });
-  }
-
-    /**
-     * Captura mediante un botón los datos copiados en el portapapeles
-     * ------------------------------------------------------------------
-     * Nota: El funcionamiento puedo variar dependiendo del navegador
-     * Navegadores probados: Google Chrome, Firefox, Microsoft Edge
-     * ------------------------------------------------------------------
-     * Genera dos arreglos uno para los headers (Recuperados del excel),
-     * arreglos por filas
-     */
-    public hasDatos: boolean = false; //
-    public headers: string[] = []; // headers obtenidos desde el contenido copiado
-    public showInstructions: boolean = true; //Bandera para mostrar u ocultar instrucciones
-    public dataMesAgencias: any;
-    public showBtnAccion: boolean = false;
-    public verBtnConsulta: boolean = true;
-    public showTable: boolean = false; //Bandera para mostrar u ocultar tabla
-
-    public clickPaste() {
-      navigator.clipboard
-        ?.readText()
-        .then((text) => {
-          this.hasDatos = true;
-          const filas = text
-            .split("\n")
-            .map((row) => row.split("\t").map((cell) => cell.trim()));
-          const filasFiltradas = filas.filter((row) =>
-            row.some((cell) => cell.length > 0)
-          );
-          this.headers = filasFiltradas.length > 0 ? filasFiltradas.shift()! : [];
-          this.showInstructions = false;
-          this.dataMesAgencias = this.procesarCeldasCombinadas(filasFiltradas);
-          this.showBtnAccion = true;
-  
-          if (this.dataMesAgencias.length === 0) {
-            Swal.fire({
-              title: "Falta algo",
-              text: "Selecciona nuevamente el contenido a pegar",
-              buttonsStyling: false,
-              icon: "warning",
-              customClass: {
-                confirmButton: "btn btn-success px-4",
-                cancelButton: "btn btn- ms-2 px-4",
-              },
-            });
-            this.showBtnAccion = false;
-            this.showTable = false;
-            this.verBtnConsulta = true;
-            return;
-          }
-          console.log(filasFiltradas);
-          console.log(this.headers);
-          console.log(this.dataMesAgencias);
-        })
-        .catch((err) => {
-          console.error("Error al leer del portapapeles:", err);
-        });
-    }
-
-      /**
-   * Procesa el contenido de "filas filtradas"
-   * y genera el formato para visualizar la tabla,
-   * se agrega la propiedad colspan y el valor de la celda es value
-   * ------------------------------------------------------------------
-   */
-  private procesarCeldasCombinadas(filas: string[][]): { value: string; colspan: number }[][] {
-    return filas.map((fila) => {
-      let filaProcesada: { value: string; colspan: number }[] = [];
-      let celdaAnterior = "";
-      for (let colIndex = 0; colIndex < fila.length; colIndex++) {
-        let celda = fila[colIndex];
-        if (celda === "" && celdaAnterior !== "") {
-          filaProcesada[filaProcesada.length - 1].colspan += 1;
-        } else {
-          filaProcesada.push({ value: celda, colspan: 1 });
-          celdaAnterior = celda;
-        }
-      }
-      return filaProcesada;
     });
   }
 
   /**
-   * Maneja el evento y el valor del select Mes
+   * Captura mediante un botón los datos copiados en el portapapeles
+   * ------------------------------------------------------------------
+   * Nota: El funcionamiento puedo variar dependiendo del navegador
+   * Navegadores probados: Google Chrome, Firefox, Microsoft Edge
+   * ------------------------------------------------------------------
+   * Genera dos arreglos uno para los headers (Recuperados del excel),
+   * arreglos por filas
    */
-  public selecAnio: boolean = false;
+  public clickPaste() {
+    const fecha = `${this.anio}-${this.mes.toString().padStart(2, "0")}-01`;
+    navigator.clipboard
+      ?.readText()
+      .then((text) => {
+        this.hasDatos = true;
+        const filas = text
+          .split("\n")
+          .map((row) => row.split("\t").map((cell) => cell.trim()));
+        const filasFiltradas = filas.filter((row) =>
+          row.some((cell) => cell.length > 0)
+        );
+        this.headers = filasFiltradas.length > 0 ? filasFiltradas.shift()! : [];
+        this.showInstructions = false;
+        this.dataMesAgencias = filasFiltradas;
+
+        // Transformar en un array de objetos
+        const result = filasFiltradas.map((row) => {
+          const object = this.headers.reduce((acc, header, index) => {
+            if (header === "Planta") {
+              const estacion = this.catEmpresas.find((e) => e.nombre === row[index]);
+              acc["sucursales_id"] = estacion ? estacion.id : null; // Usar el ID o null si no coincide
+            } else {
+              acc[header] = row[index];
+            }
+            return acc;
+          }, {} as Record<string, string | number | boolean | null>);
+          object["fecha"] = fecha;
+          object["isNew"] = !this.existInfo
+          return object;
+        });
+
+        this.dataToSend = result;
+        this.showBtnAccion = true;
+        this.mostrar = true;
+
+        if (this.dataMesAgencias.length === 0) {
+          Swal.fire({
+            title: "Falta algo",
+            text: "Selecciona nuevamente el contenido a pegar",
+            buttonsStyling: false,
+            icon: "warning",
+            customClass: {
+              confirmButton: "btn btn-success px-4",
+              cancelButton: "btn btn- ms-2 px-4",
+            },
+          });
+          this.mostrar = false;
+          this.showBtnAccion = false;
+          this.showTable = false;
+          this.verBtnConsulta = true;
+          return;
+        }
+
+      })
+      .catch((err) => {
+        console.error("Error al leer del portapapeles:", err);
+      });
+  }
+
+  /**
+   * ------------------------------------------
+   * Si anio y  mes tiene un valor valido se muestra
+   * el botón de consulta
+   * ------------------------------------------
+   * Valida que el select tenga un valor valido
+   * @param value valor que recupera del select
+   */
   public onSelectedAnio(value: number) {
     this.anio = value;
-    this.selecAnio = true;
+    if (this.anio == 0) {
+      this.selecAnio = false;
+    } else {
+      this.selecAnio = true;
+      this.verBtnConsulta = true;
+      this.showBtnAccion = false;
+    }
   }
 
   /**
-   * Maneja el evento y el valor del select Mes
+   * Valida que el select tenga un valor valido
+   * @param value valor que recupera del select
    */
-  public selecMes: boolean = false;
   public onSelectedMes(value: number) {
     this.mes = value;
-    this.selecMes = true;
+    if (this.mes == 0) {
+      this.selecMes = false;
+    } else {
+      this.selecMes = true;
+      this.verBtnConsulta = true;
+      this.showBtnAccion = false;
+    }
   }
 }
