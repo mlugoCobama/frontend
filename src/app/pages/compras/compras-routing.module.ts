09192d52a538d7585729966e614f3de255228b88
desarrollo01Cob <desarrollo_01@cobama.com.mx>
2025-05-08 11:40:56 -0600

    Fix: Menu lateral, actualizacion componentes

--- src/app/pages/compras/compras-routing.module.ts
+++ src/app/pages/compras/compras-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ComprasComponent } from './compras/compras.component';
 import { ProveedoresComponent } from './proveedores/proveedores.component';
 import { CatUnidadesMedidasComponent } from './cat-unidades-medidas/cat-unidades-medidas.component';
-import { DetallesSolicitudCompraComponent } from './detalles-solicitud-compra/detalles-solicitud-compra.component';
+// import { DetallesSolicitudCompraComponent } from './detalles-solicitud-compra/detalles-solicitud-compra.component';
 
 const routes: Routes = [
   { path: '', component: ComprasComponent },
   { path: 'proveedores', component: ProveedoresComponent },
   { path: 'cat-unidades-medidas', component: CatUnidadesMedidasComponent },
-  { path: 'detalles-solicitud-compra', component: DetallesSolicitudCompraComponent},
+  // { path: 'detalles-solicitud-compra', component: DetallesSolicitudCompraComponent},
 ];
 
 @NgModule({
